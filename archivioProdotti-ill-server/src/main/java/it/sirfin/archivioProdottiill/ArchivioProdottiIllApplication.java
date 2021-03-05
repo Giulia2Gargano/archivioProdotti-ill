e89471a337488b6aabf6f90f4e70c7103d64afd6
Emanuele1927 <35915808+Emanuele1927@users.noreply.github.com>
2021-03-05 15:05:39 +0100

    Creato lato server

---
+++ archivioProdotti-ill-server/src/main/java/it/sirfin/archivioProdottiill/ArchivioProdottiIllApplication.java
@@ -0,0 +1,13 @@
+package it.sirfin.archivioProdottiill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchivioProdottiIllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchivioProdottiIllApplication.class, args);
+	}
+
+}
---
+++ archivioProdotti-ill-server/src/test/java/it/sirfin/archivioProdottiill/ArchivioProdottiIllApplicationTests.java
@@ -0,0 +1,13 @@
+package it.sirfin.archivioProdottiill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchivioProdottiIllApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
